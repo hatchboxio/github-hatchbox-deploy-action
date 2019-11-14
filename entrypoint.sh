32b6d3bcb6fffd4b19e5af1199cd2b4c6c98c109
Kevin Webster <kevin@kevinwebster.me>
2019-11-14 13:19:15 -0800

    Getting real

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/sh -l
 
-env
+curl \
+  $INPUT_ENDPOINT \
+  -F api_key=$INPUT_API_KEY \
+  -F deploy[environment]=$INPUT_ENVIRONMENT \
+  -F deploy[local_username]=$GITHUB_ACTOR \
+  -F deploy[revision]=$GITHUB_SHA \
+  -F deploy[repository]=$GITHUB_REPOSITORY
