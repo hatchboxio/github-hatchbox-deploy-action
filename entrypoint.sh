6a776279251bc489986dab6baa8ac99bf6242dec
Kevin Webster <kevin@kevinwebster.me>
2019-11-14 13:30:38 -0800

    Use full github url for repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ curl \
   -F deploy[environment]=$INPUT_ENVIRONMENT \
   -F deploy[local_username]=$GITHUB_ACTOR \
   -F deploy[revision]=$GITHUB_SHA \
-  -F deploy[repository]=$GITHUB_REPOSITORY
+  -F deploy[repository]=git@github.com:$GITHUB_REPOSITORY.git
