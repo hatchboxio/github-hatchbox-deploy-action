cf2323487b680e076fd08d03e59541f732c6681b
pjcoenen <pieterjan.coenen@invitae.com>
2022-01-24 12:36:06 +0100

    Added support for hatchbox classic and action will fail if deployment link failed

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,22 @@
 #!/bin/sh -l
 
-curl -sS \
-  https://www.hatchbox.io/webhooks/custom/push/$INPUT_DEPLOY_KEY?ref=refs%2Fheads%2F$INPUT_BRANCH
+if [[ $INPUT_CLASSIC == "true" ]]; then
+  url_start="classic"
+else
+  url_start="WWW"
+fi
+
+echo "https://$url_start.hatchbox.io/webhooks/custom/push/$INPUT_DEPLOY_KEY?ref=refs%2Fheads%2F$INPUT_BRANCH"
+
+result=$(curl https://$url_start.hatchbox.io/webhooks/custom/push/$INPUT_DEPLOY_KEY?ref=refs%2Fheads%2F$INPUT_BRANCH)
+
+echo "$result"
+
+if [[ $result =~ "\"success\":true" ]]; then
+  echo "Success"
+  exit 0
+else
+  echo "Failed"
+  exit 1
+fi
+
