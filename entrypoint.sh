d62f97c662b4c97371c58cc49b4063a16cd4e468
Chris Oliver <excid3@gmail.com>
2023-09-15 12:25:45 -0500

    Add response json to github outputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,6 @@
 
 set -xe
 
-curl -X POST --fail-with-body "https://app.hatchbox.io/webhooks/deployments/$INPUT_DEPLOY_KEY?sha=$INPUT_SHA"
+response=curl -X POST --fail-with-body "https://app.hatchbox.io/webhooks/deployments/$INPUT_DEPLOY_KEY?sha=$INPUT_SHA"
+echo "response=$response" >> $GITHUB_OUTPUT
 echo "Deployment has started on Hatchbox.io"
